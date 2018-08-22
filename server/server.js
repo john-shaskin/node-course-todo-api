cb8a10957e0042191c2ca574b276c2b862ecda97
John Shaskin <guitarjohn@gmail.com>
2018-08-21 23:30:18 -0600

    Add GET /todos/:id endpoint

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
-const {mongoose} = require('./db/mongoose')
+const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
@@ -31,6 +32,31 @@ app.get('/todos', (req, res) => {
     res.status(400);
     res.send(e);
   })
+});
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+  var todoId = req.params.id || '';
+  if (!ObjectId.isValid(todoId)) {
+    res.status(404);
+    res.send();
+  }
+  else {
+    Todo.findById(todoId).then(todo => {
+      if (todo === null) {
+        res.status(404);
+        res.send();
+      }
+      else {
+        res.status(200);
+        res.send({todo});
+      }
+    }).catch(e => {
+      res.status(400);
+      res.send(e);
+    });
+  }
+  //   error -> 400 with error body
 });
 
 app.listen(3000, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,10 +1,12 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
 const todos = [{
+  _id: new ObjectId('DEADBEEFDEADBEEFDEADBEEF'),
   text: 'Something thingered'
 }, {
   text: 'This is a thing also'
@@ -69,4 +71,41 @@ describe('GET /todos', () => {
       })
       .end(done);
   })
+});
+
+describe('GET /todos/:id', () => {
+  it('should return 404 if ID malformed', (done) => {
+    request(app)
+      .get('/todos/where-is-the-beef')
+      .expect(404)
+      .expect(res => {
+        expect(res.body).toNotExist;
+      })
+      .end(done);
+  });
+
+  it('should return 404 if ID okay but no TODO exists', (done) => {
+    request(app)
+      .get('/todos/000000000000000000000000')
+      .expect(404)
+      .expect(res => {
+        expect(res.body).toNotExist;
+      })
+      .end(done);
+  });
+
+  it ('should return a TODO by ID, if it exists', (done) => {
+    request(app)
+    .get('/todos/DEADBEEFDEADBEEFDEADBEEF')
+    .expect(200)
+    .expect(res => {
+      expect(res.body.todo.text).toBe('Something thingered');
+    })
+    .end(done);
+  });
+
+  it ('should return 400 if there was an error', (done) => {
+    expect(true).isTrue;
+    done();
+  });
 });
